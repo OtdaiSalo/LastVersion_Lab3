9ea21bd493ef476ef6a400748216125dd847664f
OtdaiSalo <kahanyak18@gmail.com>
2018-11-21 22:33:47 +0200

    I have imroped my code style

--- Lab3.c
+++ Lab3.c
@@ -1,27 +1,27 @@
 #include <math.h>
 #include <stdio.h>
-#define i 5 
-#define j 5 
+#define rows 5 
+#define cols 5 
 
-void inputArray(int mass[][j]){
-   for(int q=0; q<5; q++){
-       for(int e=0; e<5; e++){
-           printf("¬вед≥ть елемент матриц≥ а[%d][%d] ", q, e);
+void inputUnsortedArray(int Matrix5x5[][cols]){
+   for(int rowNumber=0; rowNumber <5; rowNumber++){
+       for(int columnNumber=0; columnNumber <5; columnNumber++){
+           printf("¬вед≥ть елемент матриц≥ Matrix5x5[%d][%d] ", rowNumber, rowNumber);
            printf("\n");
-           scanf("%d",&mass [q][e]); 
+           scanf("%d",&Matrix5x5[rowNumber][columnNumber]);
        }
    }
 }
 
-void sortArray(int mass[][j]){
-int a, b, k, x, n; 
-for (k=0; k<j; k++){
-   for (b=0; b<i-1; b++){
-       for (a=0; a<=j-1; a++){
-           if ( mass[a][b] < mass[a][b+1]){
-               x = mass[a][b];
-               mass[a][b] = mass[a][b+1];
-               mass[a][b+1] = x; 
+void bubbleSortArray(int Matrix5x5[][cols]){
+int rowNumber, columnNumber, Iteration, additionalArray;
+for (Iteration =0; Iteration <j; Iteration++){
+   for (columnNumber = 0; columnNumber < rows -1; columnNumber++){
+       for (rowNumber = 0; rowNumber <= cols-1; rowNumber++){
+           if (Matrix5x5[rowNumber][columnNumber] < Matrix5x5[rowNumber][columnNumber +1]){
+               x = Matrix5x5[rowNumber][columnNumber];
+			   Matrix5x5[rowNumber][columnNumber] = mass[rowNumber][columnNumber +1];
+			   Matrix5x5[rowNumber][columnNumber +1] = additionalArray;
            }
        }
    }
@@ -29,49 +29,45 @@ for (k=0; k<j; k++){
 }
 
 
-void printArray(int mass[][j]){
-int a1, b1;
-for(a1=0; a1<5; a1++){
-    for(b1=0; b1<5; b1++){
-    printf("%4.1d ", mass[a1][b1]);
+void printSortedArray(int Matrix5x5[][cols]){
+int columnNumber, rowNumber;
+for(columnNumber = 0; columnNumber < 5; columnNumber++){
+    for(rowNumber = 0; rowNumber  < 5; rowNumber++){
+    printf("%4.1d ¬≥дсортована матриц€ маЇ вигл€д: \n", Matrix5x5[columnNumber][rowNumber]);
 }
 printf("\n");
 }
 }
 
 
-void overDiagonal( int arr[][j]) {
-    int sum1_4 [4] = {1, 1, 1, 1};
-    int sum = 1;
-    int Func = 0;
-    for(int a=0; a<5; a++){
-        for(int b=0; b<5-a; b++){
-        sum1_4 [a] = sum1_4 [a] * arr[b][a]; 
+void printSumOverMainDiagonal( int Matrix5x5[][cols]) {
+    int sumsOverMainDiagonal [4] = {1, 1, 1, 1};
+    int sumOfSumsOverDiag = 0;
+
+    for(int columnNumber = 0; columnNumber < 5; columnNumber++){
+        for(int rowNumber = 0; rowNumber < 5 - columnNumber; rowNumber++){
+			sumsOverMainDiagonal [columnNumber] = sumsOverMainDiagonal [columnNumber] * Matrix5x5[rowNumber][columnNumber];
     }
     }
-    for(int b=0; b<4; b++){
-        sum1_4 [b] = pow(sum1_4 [b],(1.0/(4-b)));
-        printf("%d ", sum1_4 [b]);
-        Func += sum1_4 [b];
+    for(int rowNumber=0; rowNumber < 4; rowNumber++){
+		sumsOverMainDiagonal[rowNumber] = pow(sumsOverMainDiagonal[rowNumber],(1.0/(4- rowNumber)));
+        printf("%d ", sumsOverMainDiagonal[rowNumber]);
+		sumOfSumsOverDiag += sumsOverMainDiagonal[rowNumber];
     }
     printf("\n");
-    printf("—ума дор≥внюЇ середн≥х геометричних значень = %d ",Func);
+    printf("—ума середн≥х геометричних значень дор≥внюЇ = %d ", sumOfSumsOverDiag);
 }
 
 
 void main(void){
-int mass [i][j] = {0};/* = {{0, 2, -2, 89,21},
-    {-1, -4, 36, 41, 71},
-    {56, 93, 5, -2, -51},
-    {1, 3, -8, 0, 9},
-    {23, 41, 5, 8, -2}};*/
+int Matrix5*5 [i][j] = {0};
     
-int *arr = mass;
-inputArray(arr);
-sortArray(arr);
+int *array = Matrix5*5;
+inputUnsortedArray(array);
+bubbleSortArray(array);
 printf("\n");
-printArray(arr);
+printSortedArray(array);
 printf("\n");
-overDiagonal(arr);
+printSumOverMainDiagonal(array);
 printf("\n");
 }
